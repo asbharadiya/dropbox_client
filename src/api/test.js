2501c133ab659826bf57408ea0e0be20c25e3365
Ankit Bharadiya <asbharadiya@gmail.com>
2017-10-03 04:47:22 -0700

    login api
    login api integrated for testing

--- src/api/test.js
+++ src/api/test.js
@@ -1,3 +1,8 @@
+const api = process.env.REACT_APP_CONTACTS_API_URL || 'http://localhost:3001'
+
+const headers = {
+    'Accept': 'application/json'
+};
 
 export const getDashboardData = () =>
 	fetch('/assets/data/dashboard.json', {
@@ -7,3 +12,18 @@ export const getDashboardData = () =>
 	}).catch(error => {
         return error;
     });
+
+
+export const login = (payload) =>
+	fetch(api+'/api/login', {
+	    method: 'POST',
+	    headers: {
+	    	...headers,
+            'Content-Type': 'application/json'
+        },
+        body: JSON.stringify(payload)
+	}).then(res => {
+		return res.json();
+	}).catch(error => {
+        return error;
+    });    
--- src/components/landing/signin.js
+++ src/components/landing/signin.js
@@ -1,29 +1,47 @@
 import React, { Component } from 'react';
+import * as api from '../../api/test';
 
 class Signin extends Component {
-  	render() {
-    	return (
-      		<div className="signin-form">
-        		<div className="form-header">
-        			<p className="title">Sign in</p>
-        			<p className="subscript">
-        				or <a onClick={this.props.onSignupClick}>create an account</a>
-        			</p>
+
+  constructor(props){
+    super(props);
+    this.handleSubmit = this.handleSubmit.bind(this);
+  }
+
+  handleSubmit() {
+    api.login({"username":"test","password":"test"})
+    .then((status) => {
+      if (status === 200) {
+        this.props.history.push("/home");
+      } else if (status === 401) {
+        
+      }
+    });
+  };
+
+	render() {
+  	return (
+    		<div className="signin-form">
+      		<div className="form-header">
+      			<p className="title">Sign in</p>
+      			<p className="subscript">
+      				or <a onClick={this.props.onSignupClick}>create an account</a>
+      			</p>
+      		</div>
+      		<div className="form-body">
+        		<div className="form-group">
+        			<input type="text" className="form-control" placeholder="Email"/>
         		</div>
-        		<div className="form-body">
-	        		<div className="form-group">
-	        			<input type="text" className="form-control" placeholder="Email"/>
-	        		</div>
-	        		<div className="form-group">
-	        			<input type="password" className="form-control" placeholder="Password"/>
-	        		</div>
-	        		<div className="form-group btn-container">
-	        			<button className="btn btn-primary btn-dropbox">Sign in</button>
-	        		</div>
+        		<div className="form-group">
+        			<input type="password" className="form-control" placeholder="Password"/>
+        		</div>
+        		<div className="form-group btn-container">
+        			<button className="btn btn-primary btn-dropbox" onClick={this.handleSubmit}>Sign in</button>
         		</div>
       		</div>
-    	);
-  	}
+    		</div>
+  	);
+	}
 }
 
 export default Signin;
